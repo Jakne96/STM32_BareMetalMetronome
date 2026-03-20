40f83711bfce936717a82b1e90fb7d96da65e6e1
Jakne96 <janeczek999666@gmail.com>
2026-03-20 15:02:35 +0100

    Update gpio.c

--- Src/gpio.c
+++ Src/gpio.c
@@ -8,25 +8,17 @@
 #include "gpio.h"
 #define GPIOAEN (1U<<0)
 
-
 void gpio_init(void){
-//Trzeba zainicjować przyciski do zmiany stanów (PLAY, CONFIGURE, RESET), czyli trzy po prostu, ale to może jeszcze nie teraz, bo trzeba inne rzeczy zainicjować tam gdize już wiemy
-//
-	//Przyciski do zmiany stanów// PA3 PA4 PA5 //Trzeba zamienić na PA4 5 6 i dioda na 7 bo pa2 chyba koliduje z uartem
-	    //Dioda do świecenia, później do zastąpienia buzzerem, PA6
 
 	RCC->AHB1ENR |= GPIOAEN;
 
-	//Buttony
+	//Buttons
 	GPIOA->MODER &= ~(1U<<8);
 	GPIOA->MODER &= ~(1U<<9);
 	GPIOA->MODER &= ~(1U<<10);
 	GPIOA->MODER &= ~(1U<<11);
 	GPIOA->MODER &= ~(1U<<12);
 	GPIOA->MODER &= ~(1U<<13);
-	//Dioda
-	GPIOA->MODER |= (1U<<14);
-	GPIOA->MODER &= ~(1U<<15);
 
 	GPIOA->PUPDR |= (1U<<8);
 	GPIOA->PUPDR &= ~(1U<<9);
@@ -34,4 +26,9 @@ void gpio_init(void){
 	GPIOA->PUPDR &= ~(1U<<11);
 	GPIOA->PUPDR |= (1U<<12);
 	GPIOA->PUPDR &= ~(1U<<13);
+	
+	//Buzzer
+	GPIOA->MODER |= (1U<<14);
+	GPIOA->MODER &= ~(1U<<15);
+
 }
